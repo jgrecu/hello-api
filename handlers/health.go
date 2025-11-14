42f304f575733e4d67880a4e4b608ad34d345497
Jeremy Grecu <jgrecu@gmail.com>
2025-11-14 19:31:45 +0100

    Updated .golangci-lint.yaml and ran golangci-lint run --fix

--- handlers/health.go
+++ handlers/health.go
@@ -1,16 +1,16 @@
 package handlers
 
 import (
-    "encoding/json"
-    "net/http"
+	"encoding/json"
+	"net/http"
 )
 
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
-    encoder := json.NewEncoder(w)
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    resp := map[string]interface{}{"status": "up"}
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	resp := map[string]any{"status": "up"}
 
-    if err := encoder.Encode(resp); err != nil {
-        panic("unable to encode response")
-    }
+	if err := encoder.Encode(resp); err != nil {
+		panic("unable to encode response")
+	}
 }
--- translation/translator_test.go
+++ translation/translator_test.go
@@ -1,74 +1,74 @@
 package translation_test
 
 import (
-    "testing"
+	"testing"
 
-    "github.com/jgrecu/hello-api/translation"
+	"github.com/jgrecu/hello-api/translation"
 )
 
 func TestTranslate(t *testing.T) {
-    // Arrange
-    tt := []struct {
-        Word        string
-        Language    string
-        Translation string
-    }{
-        {
-            Word:        "hello",
-            Language:    "english",
-            Translation: "hello",
-        },
-        {
-            Word:        "hello",
-            Language:    "german",
-            Translation: "hallo",
-        },
-        {
-            Word:        "hello",
-            Language:    "finnish",
-            Translation: "hei",
-        },
-        {
-            Word:        "hello",
-            Language:    "dutch",
-            Translation: "",
-        },
-        {
-            Word:        "bye",
-            Language:    "dutch",
-            Translation: "",
-        },
-        {
-            Word:        " hello ",
-            Language:    "german",
-            Translation: "hallo",
-        },
-        {
-            Word:        "bye",
-            Language:    "german",
-            Translation: "",
-        },
-        {
-            Word:        "hello",
-            Language:    "swahili",
-            Translation: "swatch",
-        },
-        {
-            Word:        "hello",
-            Language:    "french",
-            Translation: "bonjour",
-        },
-    }
+	// Arrange
+	tt := []struct {
+		Word        string
+		Language    string
+		Translation string
+	}{
+		{
+			Word:        "hello",
+			Language:    "english",
+			Translation: "hello",
+		},
+		{
+			Word:        "hello",
+			Language:    "german",
+			Translation: "hallo",
+		},
+		{
+			Word:        "hello",
+			Language:    "finnish",
+			Translation: "hei",
+		},
+		{
+			Word:        "hello",
+			Language:    "dutch",
+			Translation: "",
+		},
+		{
+			Word:        "bye",
+			Language:    "dutch",
+			Translation: "",
+		},
+		{
+			Word:        " hello ",
+			Language:    "german",
+			Translation: "hallo",
+		},
+		{
+			Word:        "bye",
+			Language:    "german",
+			Translation: "",
+		},
+		{
+			Word:        "hello",
+			Language:    "swahili",
+			Translation: "swatch",
+		},
+		{
+			Word:        "hello",
+			Language:    "french",
+			Translation: "bonjour",
+		},
+	}
 
-    for _, test := range tt {
-        // Act
-        res := translation.Translate(test.Word, test.Language)
+	for _, test := range tt {
+		// Act
+		res := translation.Translate(test.Word, test.Language)
 
-        // Assert
-        if res != test.Translation {
-            t.Errorf(
-                `expected "%s" to be "%s" from "%s" but received "%s"`,
-                test.Word, test.Language, test.Translation, res)
-        }
-    }
+		// Assert
+		if res != test.Translation {
+			t.Errorf(
+				`expected "%s" to be "%s" from "%s" but received "%s"`,
+				test.Word, test.Language, test.Translation, res)
+		}
+	}
 }
