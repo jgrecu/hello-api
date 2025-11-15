606415d40dcb7928affe49587e80be0ad17e3d5e
Jeremy Grecu <jgrecu@gmail.com>
2025-11-15 13:34:16 +0100

    Implemented the dependency injection for the service and test stubbing

--- handlers/rest/translator.go
+++ handlers/rest/translator.go
@@ -4,9 +4,24 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
-
-	"github.com/jgrecu/hello-api/translation"
 )
+
+type Translator interface {
+	Translate(word string, language string) string
+}
+
+// TranslateHandler will translate calls for caller.
+type TranslateHandler struct {
+	service Translator
+}
+
+// NewTranslateHandler will create a new instance of the handler using a
+// translation service.
+func NewTranslateHandler(service Translator) *TranslateHandler {
+	return &TranslateHandler{
+		service: service,
+	}
+}
 
 type Resp struct {
 	Language    string `json:"language"`
@@ -15,7 +30,10 @@ type Resp struct {
 
 const defaultLanguage = "english"
 
-func TranslateHandler(w http.ResponseWriter, r *http.Request) {
+// TranslateHandler will take a given request with a path value of the
+// word to be translated and a query parameter of the language to
+// translate to.
+func (t *TranslateHandler) TranslateHandler(w http.ResponseWriter, r *http.Request) {
 	enc := json.NewEncoder(w)
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 
@@ -26,17 +44,17 @@ func TranslateHandler(w http.ResponseWriter, r *http.Request) {
 
 	word := strings.ReplaceAll(r.URL.Path, "/", "")
 
-	transl := translation.Translate(word, language)
-	if transl == "" {
+	translation := t.service.Translate(word, language)
+	if translation == "" {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
 	resp := Resp{
 		Language:    language,
-		Translation: transl,
+		Translation: translation,
 	}
 	if err := enc.Encode(resp); err != nil {
-		panic("unable to encode respoonse")
+		panic("unable to encode response")
 	}
 }
